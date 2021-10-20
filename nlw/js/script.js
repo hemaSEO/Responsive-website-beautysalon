aba0e6778a1bb7f94508e91d162b34eb9409fde1
daniel dantas <contatorobotdeveloper@gmail.com>
2021-10-20 20:23:47 -0300

    commit main

---
+++ nlw/js/script.js
@@ -0,0 +1,67 @@
+//DOM - DOCUMENT OBJECT MODEL
+
+/* Abre e fecha o menu quando clicar no ícone: hambúrguer e X */
+const nav = document.querySelector('#header nav') // objeto vai procurar o #header nav
+const toggle = document.querySelectorAll('nav .toggle')
+console.log(toggle) //coloca no console do F12
+
+for (const element of toggle) {
+    element.addEventListener('click', function () {
+        nav.classList.toggle('show')
+    })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('show')
+    })
+}
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= navHeight) {
+        //scroll é maior que a altura do header
+        header.classList.add('scroll')
+    } else {
+        //scroll é menor que a altura do header
+        header.classList.remove('scroll')
+    }
+})
+
+/* 
+
+// char - string
+'um texto aqui'
+
+//number
+12345
+
+//boolean: true ou false
+true
+false 
+
+{{
+    name:'carro',
+    cor: 'vermelho'
+    correr: function(){
+        
+    }
+}}
+
+const people = {
+    name: 'Daniel Dantas',
+    age: 20,
+    falar: function () {
+        alert(people.name)
+    }
+}
+
+people.falar()
+
+*/
